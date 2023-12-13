3af6efb0c5336acd24648073d7bb166ce78c7bb3
rubitin <halilzahid@gmail.com>
2023-12-13 20:29:25 +0100

    upd

--- 4axisfk.py
+++ 4axisfk.py
@@ -1,9 +1,13 @@
 import numpy as np
 import math as m
 
-theta1 = m.radians(0.0)
-theta2 = m.radians(0.0)
-theta3 = m.radians(0.0)
+th1 = float(input("Enter a value for theta 1: "))
+th2 = float(input("Enter a value for theta 2: "))
+th3 = float(input("Enter a value for theta 3: "))
+
+theta1 = m.radians(th1)
+theta2 = m.radians(th2)
+theta3 = m.radians(th3)
 
 Z = 362.3 # max extension at Z = 162.3 >> meaning that Z stroke is maximum 200mm
 a1 = 409
@@ -26,7 +30,7 @@ H1_2 = [[m.cos(theta2), -m.sin(theta2)*m.cos(m.radians(180)), m.sin(theta2)*m.si
 #homogeneous transformation
 H2_3 = [[m.cos(m.radians(0)), -m.sin(m.radians(0))*m.cos(m.radians(0)), m.sin(m.radians(0))*m.sin(m.radians(0)), 0],
        [m.sin(m.radians(0)), m.cos(m.radians(0))*m.cos(m.radians(0)), -m.cos(m.radians(0))*m.sin(m.radians(0)), 0],
-       [0, m.sin(m.radians(0)), m.cos(m.radians(0)), a4 + a1 + a5 - Z],
+       [0, m.sin(m.radians(0)), m.cos(m.radians(0)), a4 + d],
        [0,0,0,1]]
 #homogeneous transformation
 H3_4 = [[m.cos(theta3), -m.sin(theta3)*m.cos(m.radians(0)), m.sin(theta3)*m.sin(m.radians(0)), 0],
@@ -40,4 +44,4 @@ H0_4 = np.dot(H0_2, H2_4)
 
 print(np.matrix(H0_4))
 print(" ")
-print( "d = d" , d)
+print("d = ", d)
